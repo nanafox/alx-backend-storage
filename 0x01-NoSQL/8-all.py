c249b7e16c94b73ff0dc5b3c5e21364e55dc5651
theLazyProgrammer <48143641+nanafox@users.noreply.github.com>
2024-07-15 21:26:05 +0000

    chore: Update code files to fix checker errors

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -5,16 +5,11 @@ This module contains a single function that lists all documents in NoSQL
 collection.
 """
 
-from pymongo.collection import Collection
-
 
 def list_all(mongo_collection):
     """Lists all the documents in a collection."""
 
-    if not isinstance(mongo_collection, Collection):
-        raise TypeError("mongo_collection must be a collection")
-
-    if mongo_collection.count_documents({}) == 0:
+    if mongo_collection is None:
         return []
 
     return mongo_collection.find({})
--- 0x01-NoSQL/9-insert_school.py
+++ 0x01-NoSQL/9-insert_school.py
@@ -5,13 +5,8 @@ This module contains a single function that inserts a new document in a
 collection based on the key-value pairs provided.
 """
 
-from pymongo.collection import Collection
-
 
 def insert_school(mongo_collection, **kwargs):
     """Inserts a new school into the collection and returns the id."""
-
-    if not isinstance(mongo_collection, Collection):
-        raise TypeError("mongo_collection must be a collection")
 
     return mongo_collection.insert_one(kwargs).inserted_id
